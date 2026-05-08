c000c1f74ba33cc313393bd7dd8aa5817379093f
kuan <rueikuanwei@gmail.com>
2026-05-07 23:24:17 -0700

    added logging of selected text

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
-import { Notice, Plugin } from 'obsidian';
+import { Plugin, MarkdownView } from 'obsidian';
 
 export default class DoubleChecker extends Plugin {
     async onload() {
+        const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (activeView) {
+            const editor = activeView.editor;
+
+            const selectedText = editor.getSelection();
+            if (selectedText) {
+                console.log('Selected text:', selectedText);
+            } else {
+                console.log('No text selected.');
+            }
+        }
+
+        this.addCommand({
+            id: 'check-double',
+            name: 'Check Double',
+            callback: () => {
+                const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (activeView) {
+                    const editor = activeView.editor;
+                    const selectedText = editor.getSelection();
+                    if (selectedText) {
+                        console.log('Selected text:', selectedText);
+                    } else {
+                        console.log('No text selected.');
+                    }
+                }
+            }
+        });
     }
 
     onunload() {
